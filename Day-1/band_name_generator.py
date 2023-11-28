a9f3a32ea04d9188648b296535bc55fc8e4e321f
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-11-28 14:43:51 +0530

    Day 1 completed

---
+++ Day-1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city_name=(input("Enter City Name: "))
+pet_name=(input("Enter Pet Name: "))
+print("Your Band Name Could be",city_name +" " +  pet_name)
---
+++ Day-1/question.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+print("Before Swapping:-")
+print("a:",a)
+print("b:",b)
+
+c=a
+a=b
+b=c
+print("After Swapping:-")
+print("a:",a)
+print("b:",b)
