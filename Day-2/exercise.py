dad42765f1c100b6179e349b8f16c04d2fa57777
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-11-29 12:07:42 +0530

    Day 2 started and done some exercises

---
+++ Day-2/exercise.py
@@ -0,0 +1,5 @@
+two_digit_number=(input("enter two_digit_number:"))
+
+result=int(two_digit_number[0])+int(two_digit_number[1])
+print(result)
+
---
+++ Day-2/main.py
@@ -0,0 +1,15 @@
+# # # # print("Hello"[4])
+
+
+# # # #integer
+# # # # print(123 + 345)
+# # # print(type(3.14))
+# # # 
+# # num_char=len(input("Enter your name:"))
+# # new_num_char=str(num_char)
+# # print("Your name has " + new_num_char + " characters")
+
+# a=1234
+# print(type(a))
+# print(70 + float("100.5"))
+# print(str(70)+str(100))
