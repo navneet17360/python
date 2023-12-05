1e970be8d469c258a03503e480c42d54a4fcb041
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-12-05 09:25:25 +0530

    love calculator problem

---
+++ Day-3/Love_calculator_exercise.py
@@ -0,0 +1,29 @@
+
+name1=input("enter name1")
+name2=input("enter name2")
+combined_name=name1+name2
+combined_name_lower=combined_name.lower()
+
+t=combined_name_lower.count('t')
+r=combined_name_lower.count('r')
+u=combined_name_lower.count('u')
+e=combined_name_lower.count('e')
+
+true=t+r+u+e
+
+l=combined_name_lower.count('l')
+o=combined_name_lower.count('o')
+v=combined_name_lower.count('v')
+e=combined_name_lower.count('e')
+
+love=l+o+v+e
+
+score=int(str(true)+str(love))
+print(score)
+
+if(score<10 or score>90):
+    
+elif(score>=40 and score<=50):
+
+else:
+    
